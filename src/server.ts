c53d276faca346c6b4e32fb1088022b480341f25
Laucha <lautaro.blancobordenabe@gmail.com>
2025-02-06 16:17:15 -0300

    mongoDB connection

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,10 @@
 import express, {Request, Response} from "express"
+import mongoose from 'mongoose';
+const mongoURI = "mongodb+srv://laucha:razer1337@user-database.ar27t.mongodb.net/?retryWrites=true&w=majority&appName=user-database";
+
+mongoose.connect(mongoURI)
+  .then(() => console.log('Conectado a MongoDB Atlas'))
+  .catch(err => console.error('Error al conectar a MongoDB:', err));
 
 const app = express();
 const port = 5000;
