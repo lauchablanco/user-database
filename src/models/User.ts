88adb7e3fd879764f2479253b3e7fdb1781fb542
Laucha <lautaro.blancobordenabe@gmail.com>
2025-02-09 11:06:37 -0300

    formatting

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,21 +1,21 @@
-import mongoose, {Schema, Document} from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
-interface IUser extends Document{
-    name:string;
-    surname:string;
-    profilePicture: string;
-    hogwartHouse: "Gryffindor" | "Slytherin" | "Hufflepuff" | "Ravenclaw";
-} 
+interface IUser extends Document {
+  name: string;
+  surname: string;
+  profilePicture: string;
+  house: "Gryffindor" | "Slytherin" | "Hufflepuff" | "Ravenclaw";
+}
 
-const UserSchema:Schema = new Schema({
-    name:{type:String, required:true},
-    surname:{type:String, required:true},
-    profilePicture:{type:String},
-    hogwartHouse:{
-        type: String,
-        enum:["Gryffindor" , "Slytherin" , "Hufflepuff" , "Ravenclaw"],
-        required:true
-    }
+const UserSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  surname: { type: String, required: true },
+  profilePicture: { type: String },
+  house: {
+    type: String,
+    enum: ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"],
+    required: true,
+  },
 });
 
 export default mongoose.model<IUser>("User", UserSchema);
