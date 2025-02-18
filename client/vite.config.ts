1d08286996a9a7c01ba523a1da611bf3c5af6e21
Laucha <lautaro.blancobordenabe@gmail.com>
2025-02-18 08:57:13 -0300

    fixing proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http//localhost:3000',
+      '/users': {
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       }
