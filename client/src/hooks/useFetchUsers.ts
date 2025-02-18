e5e0a63e8e9c57cd466ea753c5d9f26074cb6da6
Laucha <lautaro.blancobordenabe@gmail.com>
2025-02-18 08:57:32 -0300

    fetch user hook

---
+++ client/src/hooks/useFetchUsers.ts
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { resolveErrorMessage } from "../utils/errorsUtils";
+import { User } from "common-types";
+
+type UseFetchUsersResult = {
+    users: User[];
+    loading: boolean;
+    error: string | null;
+    refetch: () => Promise<void>;
+};
+
+export const useFetchUsers = () : UseFetchUsersResult => {
+    const [users, setUsers] = useState<User[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    const fetchUsers = async () : Promise<void>  => {
+        setLoading(true);
+        try {
+            const response = await fetch('/users');  // With the proxy it's gonna be http://localhost:5000/users
+            if (!response.ok) {
+                throw new Error(await resolveErrorMessage(response));
+            }
+
+            const data = await response.json();
+            setUsers(data);
+            setError(null);
+
+        } catch (error) {
+            console.error("Error fetching users:", error);
+            setError(error instanceof Error ? error.message : "Unknown error");
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    return { users, loading, error, refetch: fetchUsers };
+}
