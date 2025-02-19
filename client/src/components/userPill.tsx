69847cdebf3e787d8f5535b038156771c47fc53c
Laucha <lautaro.blancobordenabe@gmail.com>
2025-02-19 19:22:55 -0300

    css styles

--- client/src/components/userPill.tsx
+++ client/src/components/userPill.tsx
@@ -1,12 +1,24 @@
-import '../styles/userPill.css'
-import { User } from 'common-types';
+import React from "react";
+import "../styles/userPill.css"; // Importamos los estilos
+import { User } from "common-types";
 
-interface UserPillProps {
-    user: User; 
-}
+const UserPill: React.FC<{ user: User }> = ({ user }) => {
+    const defaultProfilePic = "../../public/default-profile-man.jpg"; // Imagen local en public/images
+  return (
+    <div className={`user-pill ${user.house.toLowerCase()}`}>
+      {/* Foto de perfil */}
+      <img src={defaultProfilePic} alt={user.name} className="profile-pic" />
 
-const UserPill : React.FC<UserPillProps> = ({user}) => {
-    return <div className={'userPill'} id={user._id}>{user.name + " " + user.surname}</div>
-}
+      {/* Info de usuario */}
+      <div className="user-info">
+        <p className="user-name">{user.name} {user.surname}</p>
+        <p className="user-email">{user.surname}</p>
+      </div>
+
+      {/* Casa de Hogwarts */}
+      <span className="user-house">{user.house}</span>
+    </div>
+  );
+};
 
 export default UserPill;
