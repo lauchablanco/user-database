edebc34422192640e81fc8986910d3c7642af02d
Laucha <lautaro.blancobordenabe@gmail.com>
2025-02-19 23:46:26 -0300

    fix on userPill, now is always showing reload button

--- client/src/components/userList.tsx
+++ client/src/components/userList.tsx
@@ -3,21 +3,23 @@ import { useFetchUsers } from '../hooks/useFetchUsers';
 import UserPill from './userPill';
 
 const UserList: React.FC = () => {
-    
-    const { users, loading, error, refetch } = useFetchUsers();
 
-    if (loading) return <p>Loading users...</p>;
-    if (error) return <p>Error: {error}</p>;
+    const { users, loading, error, refetch } = useFetchUsers();
 
     return (
         <div>
             <h2>Users List</h2>
             <button onClick={refetch}>Reload</button>
-            <ul>
-                {users.map((user) => (
-                    <UserPill user={user} />
-                ))}
-            </ul>
+            {loading && <p>Loading users...</p>}
+            {error && <p>Error: {error}</p>}
+
+            {!loading && !error && (
+                <ul>
+                    {users.map((user) => (
+                        <UserPill key={user._id} user={user} />
+                    ))}
+                </ul>
+            )}
         </div>
     );
 };
