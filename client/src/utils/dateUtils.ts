0020114b03e43a27ca406fdfc0624927bf3b414b
Laucha <lautaro.blancobordenabe@gmail.com>
2025-02-22 00:34:28 -0300

    dateFormatter function

---
+++ client/src/utils/dateUtils.ts
@@ -0,0 +1,7 @@
+export const dateFormatter = (fecha: string | Date): string => {
+    return new Date(fecha).toLocaleDateString("es-AR", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+  };
