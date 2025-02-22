84bd3b157c43bf572dad2d6ee9c65b01040ec8a6
Laucha <lautaro.blancobordenabe@gmail.com>
2025-02-22 02:54:09 -0300

    filter by name

--- client/src/components/userList.tsx
+++ client/src/components/userList.tsx
@@ -6,23 +6,36 @@ import { User } from 'common-types';
 
 const UserList: React.FC = () => {
 
-    const { users, loading, error, refetch } = useFetchUsers();
+    const { fetchedUsers, loading, error, refetch } = useFetchUsers();
     const [selectedUser, setSelectedUser] = useState<User | null>(null);
-    const handleUserClick = (user:User) => {
-      setSelectedUser(user);
+    const [nameFilter, setNameFilter] = useState<string>('');
+
+    const handleFilterNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNameFilter(e.target.value);
+    }
+
+    const handleUserClick = (user: User) => {
+        setSelectedUser(user);
     };
-    
+
+    const filteredUsers = fetchedUsers.filter(user => user.fullName.toLowerCase().includes(nameFilter.toLowerCase()));
+
     return (
-        <div>
+        <div className="user-list-container">
             <h2>Users List</h2>
-            <button onClick={refetch}>Reload</button>
+            <div className="user-list-header">
+                <button onClick={refetch}>Reload</button>
+            </div>
+            <div className="user-list-filter">
+                <input placeholder='Filter by Name' value={nameFilter} onChange={(e) => handleFilterNameChange(e)}></input>
+            </div>
             {loading && <p>Loading users...</p>}
             {error && <p>Error: {error}</p>}
 
-            {!loading && !error && (
+            {!loading && !error && filteredUsers && (
                 <ul>
-                    {users.map((user) => (
-                        <UserPill key={user._id} user={user} onClick={() => handleUserClick(user)}/>
+                    {filteredUsers.map((user: User) => (
+                        <UserPill key={user._id} user={user} onClick={() => handleUserClick(user)} />
                     ))}
                 </ul>
             )}
