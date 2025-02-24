19ea1cfde6777368f0f118a83bcb11c7e69e8b1b
Laucha <lautaro.blancobordenabe@gmail.com>
2025-02-24 14:01:13 -0300

    Filter generic component

---
+++ client/src/components/Filter.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Select from 'react-select';
+import { FilterOption } from '../types/filterOption';
+
+interface UserFilterProps {
+    options: FilterOption[];
+    filterName: string;
+    onSelectedOptionChange: (selectedOptions: FilterOption[]) => void;
+}
+
+const UserFilter: React.FC<UserFilterProps> = ({ options, filterName, onSelectedOptionChange }: UserFilterProps) => {
+    const [selectedOptions, setSelectedOptions] = useState<FilterOption[]>([]);
+
+
+    const handleChange = (selectedOptions: any) => {
+        setSelectedOptions(selectedOptions || []);
+        onSelectedOptionChange(selectedOptions);
+    };
+
+    return <div>
+        <label>{filterName}</label>
+        <Select
+            id="filterName"
+            options={options}
+            value={selectedOptions}
+            isMulti
+            onChange={handleChange}
+        />
+    </div>
+}
+
+export default UserFilter;
