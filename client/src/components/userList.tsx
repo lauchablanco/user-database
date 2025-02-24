ae2e46944d145abe28d71d9ab429e6dc48294b8d
Laucha <lautaro.blancobordenabe@gmail.com>
2025-02-24 14:01:49 -0300

    House filter in user list component

--- client/src/components/userList.tsx
+++ client/src/components/userList.tsx
@@ -1,24 +1,53 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useFetchUsers } from '../hooks/useFetchUsers';
 import UserPill from './UserPill';
 import UserModal from './UserModal';
-import { User } from 'common-types';
+import { User, House } from 'common-types';
+import UserFilter from './Filter';
+import { FilterOption } from '../types/filterOption';
 
 const UserList: React.FC = () => {
 
     const { fetchedUsers, loading, error, refetch } = useFetchUsers();
     const [selectedUser, setSelectedUser] = useState<User | null>(null);
+    const [filteredUsers, setFilteredUsers] = useState<User[] | null>(fetchedUsers);
     const [nameFilter, setNameFilter] = useState<string>('');
+    const [houseFilter, setHouseFilter] = useState<House[]>([]);
 
     const handleFilterNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setNameFilter(e.target.value);
+        const nameFilter = e.target.value;
+        setNameFilter(nameFilter);
     }
 
     const handleUserClick = (user: User) => {
         setSelectedUser(user);
     };
 
-    const filteredUsers = fetchedUsers.filter(user => user.fullName.toLowerCase().includes(nameFilter.toLowerCase()));
+    const handleSelectedHouseChange = (selectedHouses: FilterOption[]) => {
+        const selectedHousesFilter = selectedHouses.map(sh => sh.label as House);
+        setHouseFilter(selectedHousesFilter);
+    }
+
+    useEffect(() => { setFilteredUsers(fetchedUsers) }, []);
+
+
+    useEffect(() => {
+        let result = fetchedUsers;
+        // name filter
+        if (nameFilter) {
+            result = result.filter((user) =>
+                user.fullName.toLowerCase().includes(nameFilter.toLowerCase())
+            );
+        }
+
+        // house filter
+        if (houseFilter.length > 0) {
+            result = result.filter((user) => houseFilter.includes(user.house));
+        }
+
+        setFilteredUsers(result);
+    }, [nameFilter, houseFilter, fetchedUsers]);  // on filters change
+
 
     return (
         <div className="user-list-container">
@@ -28,6 +57,7 @@ const UserList: React.FC = () => {
             </div>
             <div className="user-list-filter">
                 <input placeholder='Filter by Name' value={nameFilter} onChange={(e) => handleFilterNameChange(e)}></input>
+                <UserFilter filterName='house' options={Object.values(House).map(house => { return { value: house.toLowerCase(), label: house } })} onSelectedOptionChange={handleSelectedHouseChange}></UserFilter>
             </div>
             {loading && <p>Loading users...</p>}
             {error && <p>Error: {error}</p>}
