a59ec1a38a2090e82bdf665f7af05108dc81402f
Laucha <lautaro.blancobordenabe@gmail.com>
2025-02-25 19:11:47 -0300

    new users

--- server/src/scripts/mock-data.ts
+++ server/src/scripts/mock-data.ts
@@ -1,92 +1,122 @@
 export const mockData = [
-    {
-      "fullName": "Harry Potter",
-      "email": "harry.potter@hogwarts.edu",
-      "birthDate": "1980-07-31",
-      "profilePicture": "harry.jpg",
-      "house": "Gryffindor",
-      "role": "Student",
-      "pet": "Owl",
-      "gender": "Male"
-    },
-    {
-      "fullName": "Hermione Granger",
-      "email": "hermione.granger@hogwarts.edu",
-      "birthDate": "1979-09-19",
-      "profilePicture": "hermione.jpg",
-      "house": "Gryffindor",
-      "role": "Student",
-      "pet": "Cat",
-      "gender": "Female"
-    },
-    {
-      "fullName": "Ron Weasley",
-      "email": "ron.weasley@hogwarts.edu",
-      "birthDate": "1980-03-01",
-      "profilePicture": "ron.jpg",
-      "house": "Gryffindor",
-      "role": "Student",
-      "pet": "Rat",
-      "gender": "Male"
-    },
-    {
-      "fullName": "Albus Dumbledore",
-      "email": "albus.dumbledore@hogwarts.edu",
-      "birthDate": "1881-08-30",
-      "profilePicture": "dumbledore.jpg",
-      "house": "Gryffindor",
-      "role": "Headmaster",
-      "pet": "Ferret",
-      "gender": "Male"
-    },
-    {
-      "fullName": "Severus Snape",
-      "email": "severus.snape@hogwarts.edu",
-      "birthDate": "1960-01-09",
-      "profilePicture": "snape.jpg",
-      "house": "Slytherin",
-      "role": "Professor",
-      "pet": "Frog",
-      "gender": "Male"
-    },
-    {
-      "fullName": "Draco Malfoy",
-      "email": "draco.malfoy@hogwarts.edu",
-      "birthDate": "1980-06-05",
-      "profilePicture": "draco.jpg",
-      "house": "Slytherin",
-      "role": "Student",
-      "pet": "Owl",
-      "gender": "Male"
-    },
-    {
-      "fullName": "Luna Lovegood",
-      "email": "luna.lovegood@hogwarts.edu",
-      "birthDate": "1981-02-13",
-      "profilePicture": undefined,
-      "house": "Ravenclaw",
-      "role": "Student",
-      "pet": "Ferret",
-      "gender": "Female"
-    },
-    {
-      "fullName": "Minerva McGonagall",
-      "email": "minerva.mcgonagall@hogwarts.edu",
-      "birthDate": "1935-10-04",
-      "profilePicture": "mcgonagall.jpg",
-      "house": "Gryffindor",
-      "role": "Professor",
-      "pet": "Cat",
-      "gender": "Female"
-    },
-    {
-      "fullName": "Cedric Diggory",
-      "email": "cedric.diggory@hogwarts.edu",
-      "birthDate": "1977-09-01",
-      "profilePicture": "cedric.jpg",
-      "house": "Hufflepuff",
-      "role": "Student",
-      "pet": "Frog",
-      "gender": "Male"
-    }
-  ]
+  {
+    "fullName": "Lautaro Blanco",
+    "email": "lautaro.blancobordenabe@gmail.com",
+    "birthDate": "1995-04-11",
+    "profilePicture": "lau.jpg",
+    "house": "Hufflepuff",
+    "role": "Student",
+    "pet": "Cat",
+    "gender": "Male"
+  },
+  {
+    "fullName": "Harry Potter",
+    "email": "harry.potter@hogwarts.edu",
+    "birthDate": "1980-07-31",
+    "profilePicture": "harry.jpg",
+    "house": "Gryffindor",
+    "role": "Student",
+    "pet": "Owl",
+    "gender": "Male"
+  },
+  {
+    "fullName": "Hermione Granger",
+    "email": "hermione.granger@hogwarts.edu",
+    "birthDate": "1979-09-19",
+    "profilePicture": "hermione.jpg",
+    "house": "Gryffindor",
+    "role": "Student",
+    "pet": "Cat",
+    "gender": "Female"
+  },
+  {
+    "fullName": "Ron Weasley",
+    "email": "ron.weasley@hogwarts.edu",
+    "birthDate": "1980-03-01",
+    "profilePicture": "ron.jpg",
+    "house": "Gryffindor",
+    "role": "Student",
+    "pet": "Rat",
+    "gender": "Male"
+  },
+  {
+    "fullName": "Albus Dumbledore",
+    "email": "albus.dumbledore@hogwarts.edu",
+    "birthDate": "1881-08-30",
+    "profilePicture": "dumbledore.jpg",
+    "house": "Gryffindor",
+    "role": "Headmaster",
+    "pet": "Ferret",
+    "gender": "Male"
+  },
+  {
+    "fullName": "Severus Snape",
+    "email": "severus.snape@hogwarts.edu",
+    "birthDate": "1960-01-09",
+    "profilePicture": "snape.jpg",
+    "house": "Slytherin",
+    "role": "Professor",
+    "pet": "Frog",
+    "gender": "Male"
+  },
+  {
+    "fullName": "Draco Malfoy",
+    "email": "draco.malfoy@hogwarts.edu",
+    "birthDate": "1980-06-05",
+    "profilePicture": "draco.jpg",
+    "house": "Slytherin",
+    "role": "Student",
+    "pet": "Owl",
+    "gender": "Male"
+  },
+  {
+    "fullName": "Luna Lovegood",
+    "email": "luna.lovegood@hogwarts.edu",
+    "birthDate": "1981-02-13",
+    "profilePicture": undefined,
+    "house": "Ravenclaw",
+    "role": "Student",
+    "pet": "Ferret",
+    "gender": "Female"
+  },
+  {
+    "fullName": "Minerva McGonagall",
+    "email": "minerva.mcgonagall@hogwarts.edu",
+    "birthDate": "1935-10-04",
+    "profilePicture": "mcgonagall.jpg",
+    "house": "Gryffindor",
+    "role": "Professor",
+    "pet": "Cat",
+    "gender": "Female"
+  },
+  {
+    "fullName": "Cedric Diggory",
+    "email": "cedric.diggory@hogwarts.edu",
+    "birthDate": "1977-09-01",
+    "profilePicture": "cedric.jpg",
+    "house": "Hufflepuff",
+    "role": "Student",
+    "pet": "Frog",
+    "gender": "Male"
+  },
+  {
+    "fullName": "Nearly Headless Nick",
+    "email": "It's a Ghost!",
+    "birthDate": "1401-10-31",
+    "profilePicture": "nick.jpg",
+    "house": "Gryffindor",
+    "role": "Ghost",
+    "pet": "Ferret",
+    "gender": "Male"
+  },
+  {
+    "fullName": "Percy Weasley",
+    "email": "percy.weasley@hogwarts.edu",
+    "birthDate": "1976-08-22",
+    "profilePicture": "percy.jpg",
+    "house": "Gryffindor",
+    "role": "Prefect",
+    "pet": "Owl",
+    "gender": "Male"
+  }
+]
