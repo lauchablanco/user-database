a6bf2457c93cc6b464a6d659e37d3cfde605f679
Laucha <lautaro.blancobordenabe@gmail.com>
2025-12-10 08:50:22 -0300

    implementing CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,10 @@ import dotenv from 'dotenv';
 import express, {Request, Response} from "express"
 import mongoose from 'mongoose';
 import userRoutes from "./routes/userRoutes.js";
+import cors from 'cors';
 
 dotenv.config();
-const MONGO_URI = process.env.MONGO_URI_READONLY;
+const MONGO_URI = process.env.MONGO_URI_ADMIN;
 
 if (!MONGO_URI) {
   throw new Error("❌ MONGO_URI_READONLY is not defined in .env file");
@@ -17,6 +18,13 @@ mongoose.connect(MONGO_URI)
 
 const app = express();
 const port = 5000;
+
+
+app.use(cors({
+  origin: "http://localhost:5173",   // frontend
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "x-api-key"]
+}));
 
 app.use(express.json());
 app.use("/api", userRoutes);
