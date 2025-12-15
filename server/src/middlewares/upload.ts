5d1ec93bd1e3e4f902e3c7d28dc7f50157c2a6af
Laucha <lautaro.blancobordenabe@gmail.com>
2025-12-15 17:30:33 -0300

    all for updloading images: libraries, config and folders

---
+++ server/src/middlewares/upload.ts
@@ -0,0 +1,28 @@
+import multer from "multer";
+import path from "path";
+
+// dónde se guardan los archivos
+const storage = multer.diskStorage({
+  destination: (_req, _file, cb) => {
+    cb(null, "uploads/profiles");
+  },
+  filename: (_req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1e9)}${ext}`; //sugested by ChatGPT
+    cb(null, uniqueName);
+  },
+});
+
+export const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
+  },
+  fileFilter: (_req, file, cb) => {
+    if (!file.mimetype.startsWith("image/")) {
+      cb(new Error("Only images are allowed"));
+      return;
+    }
+    cb(null, true);
+  },
+});
