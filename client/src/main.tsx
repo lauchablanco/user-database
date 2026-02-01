6236ac093392e9c84c8d5ae3cc13148b12a070dd
Laucha <lautaro.blancobordenabe@gmail.com>
2026-02-01 14:02:13 +1100

    react router install

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,14 @@ import './styles/index.css'
 import './styles/theme.css'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext.tsx'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
